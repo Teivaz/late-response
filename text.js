55cc2951a8a15795567ebc1e5429ff757925fb71
Oleksandr Kuznietsov <o.kuznietsov@gmail.com>
2016-08-21 17:08:30 +0200

    Changed css path to local server mirror

--- text.js
+++ text.js
@@ -5,7 +5,7 @@ const plain = fs.readFileSync('readme.md', 'utf-8')
 const html = 
 '<html>'+
 	'<head>'+
-		'<link rel="stylesheet" href="https://rawgit.com/sindresorhus/github-markdown-css/gh-pages/github-markdown.css"/>'+
+		'<link rel="stylesheet" href="http://static.teivaz.com/github-markdown.css"/>'+
 	'</head>'+
 	'<body class="markdown-body">' + 
 		markdown.parse(plain) + 
